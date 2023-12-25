2a4a78f352bcaad286b2e9addaacccc07c4fc650
MajdMarabe <145362156+MajdMarabe@users.noreply.github.com>
2023-12-26 01:00:05 +0200

    Update User.java

--- src/main/java/carpro/User.java
+++ src/main/java/carpro/User.java
@@ -34,7 +34,7 @@ public  String username;
 	public boolean test;
 	
 	
-public boolean login (String userna, String pass) {
+public boolean login (String userna, String pass,int i) {
 	 usercont.add(new User("shahd","258","sh@gmail.com",UserType.ADMIN));
 	 usercont.add(new User("farah","3224","fa@gmail.com",UserType.TENANT));
 	 usercont.add(new User("haya","563","sh@gmail.com",UserType.TENANT));
@@ -42,30 +42,30 @@ public boolean login (String userna, String pass) {
 	 usercont.add(new User("maha","000","sshahdyyaseen66@gmail.com",UserType.TENANT));
 	 usercont.add(new User("Majd","m","ma@gmail.com",UserType.INSTALLER));
 
-	test= check(userna, pass);
+	test= check(userna, pass,i);
 	return test;
 }
 
-	public boolean check(String username, String password) {
+	public boolean check(String username, String password, int i) {
 		logger.info(username);
 		for(User user1: usercont) {
 			if (user1.getUsername().equals(username) && user1.getPassword().equals(password)) {
 				if(user1.getType().equals(UserType.ADMIN)) {
 					Admin admin1= new Admin();
-					admin1.displayDashboard(usercont);////////////////////////////////
+					if(i!=1) admin1.displayDashboard(usercont);////////////////////////////////
 
 					return true;
 				}
 				else if(user1.getType().equals(UserType.INSTALLER)) {
 					
 					Installer Ins = new Installer ();
-					Ins.installerWork();
+					 if(i!=1) Ins.installerWork();
 
 					return true;
 				}
 				else if(user1.getType().equals(UserType.TENANT)) {
 			Tenant tenant = new Tenant(username,user1.Email,user1.password);
-			tenant.tenantPage();
+			if(i!=1) tenant.tenantPage();
 					return true;
 				}
 			}
